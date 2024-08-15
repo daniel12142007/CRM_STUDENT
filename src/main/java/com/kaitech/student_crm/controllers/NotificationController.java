cd01d148a7cff535aea80497d339a11e206631f5
AizhAn1903 <149721164+AizhAn1903@users.noreply.github.com>
2024-08-15 20:40:07 +0600

    методы update и delete в Level и маденькие изменения

--- src/main/java/com/kaitech/student_crm/controllers/NotificationController.java
+++ src/main/java/com/kaitech/student_crm/controllers/NotificationController.java
@@ -2,7 +2,9 @@ package com.kaitech.student_crm.controllers;
 
 import com.kaitech.student_crm.payload.response.NotificationResponse;
 import com.kaitech.student_crm.services.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,8 @@ import java.util.List;
 public class NotificationController {
     private final NotificationService notificationService;
 
+    @Operation(summary = "Получение уведомлений по ID Студента ")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("notifications/{studentId}")
     public List<NotificationResponse> findAllNotificationByStudent(@PathVariable Long studentId) throws AccessDeniedException {
         if (SecurityContextHolder.getContext().getAuthentication().getName().equals("anonymousUser"))
