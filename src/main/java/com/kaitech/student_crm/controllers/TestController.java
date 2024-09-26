f92e4e151b37e0097bc3f29cbfa4550d184e57ca
daniel12142007 <danijelahatzanov@gmail.com>
2024-09-26 17:19:27 +0600

    Notification

---
+++ src/main/java/com/kaitech/student_crm/controllers/TestController.java
@@ -0,0 +1,52 @@
+package com.kaitech.student_crm.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+    private final JavaMailSender javaMailSender;
+    private final ResourceLoader resourceLoader;
+    @GetMapping("test/old/{email}")
+    public String sendEmail(@PathVariable String email) throws MessagingException {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Ссылка для регистрации работает только 1 раз");
+        javaMailSender.send(message);
+        return "Success";
+    }
+
+    @GetMapping("test/new/{email}")
+    public String sendEmailNew(@PathVariable String email) throws MessagingException, IOException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+
+        String htmlContent = loadHtmlTemplate("classpath:static-html/registered.html");
+
+        helper.setFrom("KaiTech");
+        helper.setSubject("Добро пожаловать!");
+        helper.setTo(email);
+        helper.setText(htmlContent, true);
+        javaMailSender.send(mimeMessage);
+        return "Success";
+    }
+    private String loadHtmlTemplate(String path) throws IOException {
+        Resource resource = resourceLoader.getResource(path);
+        return new String(Files.readAllBytes(Paths.get(resource.getURI())));
+    }
+}
